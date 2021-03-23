ce39dc8c0b7156021aae416d56295f714554a232
Hope Gochnour <hope.gochnour@gmail.com>
2021-03-23 16:12:45 -0600

    rubocop corrections

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,3 @@
 class WelcoomeController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
-  has_many :friends
-  has_many :parties, foreign_key: :host_id
+  has_many :friends, dependent: :destroy
+  has_many :parties, foreign_key: :host_id, dependent: :destroy
   has_secure_password
 end
