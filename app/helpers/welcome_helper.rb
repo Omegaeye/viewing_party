e582b8f68ecb706ab65b684af3d255f9311d6dc8
Jenny Branham <branhamjenny@gmail.com>
2021-03-22 20:07:43 -0500

    Add title and intro message to welcome index page.
    Co-Authored-By: Khoa <46826902+Omegaeye@users.noreply.github.com>

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,6 @@
 module WelcomeHelper
+  def intro
+    "Viewing party is an application to explore movies and create
+    a viewing party event for you and your friends to watch a movie together."
+  end
 end
