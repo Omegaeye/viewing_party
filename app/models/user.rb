d0af90c99c01e15156e9aaa193b05e1c1ca43ec2
Hope Gochnour <hope.gochnour@gmail.com>
2021-03-22 19:39:34 -0600

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  has_many :friends
+  has_many :parties, foreign_key: :host_id
+  has_secure_password
 end
