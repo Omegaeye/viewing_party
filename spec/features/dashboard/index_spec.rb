195bbedbfc6bcf9276f4c929027c88583cf4d294
Khoa Nguyen <khoang323@hotmail.com>
2021-03-23 15:39:06 -0600

    Complete dashboard_index_spec

---
+++ spec/features/dashboard/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'As an authenticated user' do
+  describe 'when I visit "/dashboard" I see' do
+    before :each do
+      @user = User.create(username: "sphinx", email: "123fake@email.com", password: "password")
+      visit dashboard_index_path
+    end
+
+    it "'Welcome <username>!' at the top of page" do
+      expect(page).to have_content("Welcome #{@user.username}!")
+    end
+
+    it "A button to Discover Movies" do
+      expect(page).to have_button("Discover Movies")
+    end
+
+    it "A friends section" do
+      expect(page).to have_content("Friends:")
+    end
+
+    it "A viewing parties section" do
+      expect(page).to have_content("Parties:")
+    end
+  end
+end
