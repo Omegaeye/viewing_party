208e3948e028eb7caafdb710f91693ecbf0a1cac
Hope Gochnour <hope.gochnour@gmail.com>
2021-03-23 18:48:08 -0600

    add sad path for reging a user, update user reg to check for user's id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,18 @@ class UsersController < ApplicationController
     new_params[:email] = user_params[:email].downcase
     new_params[:username] = user_params[:username].downcase
     user = User.create(new_params)
-    if user
-      flash[:success] = "Account created successfully."
+    if user.id
+      flash[:success] = 'Account created successfully.'
       session[:user_id] = user.id
       redirect_to dashboard_index_path
     else
-      flash[:alert] = "One or more fields was taken or invalid."
+      flash[:alert] = 'One or more fields was taken or invalid.'
       render :new
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
