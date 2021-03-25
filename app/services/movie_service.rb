14c27049a544bf74cad71d744591e3446a84c4ad
Khoa Nguyen <khoang323@hotmail.com>
2021-03-24 20:07:57 -0600

    got api key
    Co-Authored-By: Jenny Branham <branhamjenny@gmail.com>
    Co-Authored-By: Hope Gochnour <hope.gochnour@gmail.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,8 +1,8 @@
 class MovieService
 
   def get_top_rated
-    response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=62812900bf70170845400c81296d9581&page=1")
-    response2 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=62812900bf70170845400c81296d9581&page=2")
+    response = connection.get("movie/top_rated?&page=1")
+    response2 = connection.get("movie/top_rated?&page=2")
 
     json1 = JSON.parse(response.body, symbolize_names: true)
     json2 = JSON.parse(response2.body, symbolize_names: true)
@@ -10,10 +10,14 @@ class MovieService
   end
 
   def get_by_title(movie_title)
-    response = Faraday.get("https://api.themoviedb.org/3/search/movie?api_key=62812900bf70170845400c81296d9581&query=#{movie_title}")
+    response = connection.get("search/movie?query=#{movie_title}")
 
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
+  private
 
+  def connection
+    Faraday.new(url: "https://api.themoviedb.org/3/", params: {api_key: ENV["movie_db_api_key"]})
+  end
 end
