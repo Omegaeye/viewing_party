987b796b5eb69b7f8c5a4477c6932c96099845b3
Khoa Nguyen <khoang323@hotmail.com>
2021-03-24 19:49:56 -0600

    Got get by title method working
    Co-Authored-By: Jenny Branham <branhamjenny@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,10 @@
 class MoviesController < ApplicationController
   def index
     @movie_service = MovieService.new
-    @movies = @movie_service.get_top_rated.map{|data| MovieApi.new(data)}
+    if params[:top_rated]
+      @movies = @movie_service.get_top_rated.map{|data| MovieApi.new(data)}
+    else
+      @movies = @movie_service.get_by_title(params[:movie_title])[:results].map{|data| MovieApi.new(data)}
+    end
   end
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,15 +1,19 @@
 class MovieService
 
   def get_top_rated
-    response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=62812900bf70170845400c81296d9581&language=en-US&page=1")
-    response2 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=62812900bf70170845400c81296d9581&language=en-US&page=2")
+    response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=62812900bf70170845400c81296d9581&page=1")
+    response2 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=62812900bf70170845400c81296d9581&page=2")
 
     json1 = JSON.parse(response.body, symbolize_names: true)
     json2 = JSON.parse(response2.body, symbolize_names: true)
     json1[:results] + json2[:results]
   end
 
-  def get_by_title
-    response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=62812900bf70170845400c81296d9581&language=en-US&page=1")
+  def get_by_title(movie_title)
+    response = Faraday.get("https://api.themoviedb.org/3/search/movie?api_key=62812900bf70170845400c81296d9581&query=#{movie_title}")
+
+    json = JSON.parse(response.body, symbolize_names: true)
   end
+
+
 end
