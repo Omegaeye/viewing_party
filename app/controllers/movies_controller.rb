89aeaee84d14e5d0ef3d17da5d02d033581578d8
Jenny Branham <branhamjenny@gmail.com>
2021-03-25 10:30:20 -0500

    Add s to a word 😃

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
     @movies = if params[:top_rated]
                 movie_service.top_rated_movies.map { |data| MovieApi.new(data) }
               else
-                movie_service.movie_by_title(params[:movie_title])[:results].map { |data| MovieApi.new(data) }
+                movie_service.movies_by_title(params[:movie_title])[:results].map { |data| MovieApi.new(data) }
               end
   end
 end
