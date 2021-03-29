51c98d0728ac4849cbfa8604d27ccf21f456dcf6
Hope Gochnour <hope.gochnour@gmail.com>
2021-03-29 17:27:34 -0600

    update connection method, make it private

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,7 +1,7 @@
 class MovieService
   def self.top_rated_movies
-    response = connection.get('movie/top_rated?&page=1')
-    response2 = connection.get('movie/top_rated?&page=2')
+    response = conn.get('movie/top_rated?&page=1')
+    response2 = conn.get('movie/top_rated?&page=2')
 
     json1 = JSON.parse(response.body, symbolize_names: true)
     json2 = JSON.parse(response2.body, symbolize_names: true)
@@ -9,26 +9,27 @@ class MovieService
   end
 
   def self.movies_by_title(movie_title)
-    response = connection.get("search/movie?query=#{movie_title}")
+    response = conn.get("search/movie?query=#{movie_title}")
     JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
   def self.movie_by_id(id)
-    response = connection.get("movie/#{id}")
+    response = conn.get("movie/#{id}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.cast(movie_id)
-    response = connection.get("movie/#{movie_id}/credits")
+    response = conn.get("movie/#{movie_id}/credits")
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.reviews(movie_id)
-    response = connection.get("movie/#{movie_id}/reviews")
+    response = conn.get("movie/#{movie_id}/reviews")
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.connection
+  private
+  def self.conn
     Faraday.new(url: 'https://api.themoviedb.org/3/', params: { api_key: ENV['movie_db_api_key'] })
   end
 end
