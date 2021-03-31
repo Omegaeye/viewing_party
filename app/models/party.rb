621c75ad7cc927c0f8c62fbc4e8e46c1b832b744
Jenny Branham <branhamjenny@gmail.com>
2021-03-30 19:29:44 -0500

    Fix rubocop

--- app/models/party.rb
+++ app/models/party.rb
@@ -6,7 +6,7 @@ class Party < ApplicationRecord
   validates :duration, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   before_save :convert_date
-  
+
   def viewer_usernames
     usernames = party_viewers.map { |slot| slot.viewer.username }
     case usernames.count
