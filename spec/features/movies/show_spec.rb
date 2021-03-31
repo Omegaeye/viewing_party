f11794c9f9d4776691f1f00a9d09039e8fc08b7d
Hope Gochnour <hope.gochnour@gmail.com>
2021-03-31 12:28:46 -0600

    update spec to fix block not ending correctly after merge

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -5,7 +5,7 @@ describe "Movie Show Page" do
     before :each do
       VCR.use_cassette("show-movie") do
         data = MovieService.movie_by_id(24428)
-        @movie = MovieApi.new(data)
+        @movie = Film.new(data)
         @highfive = User.create!(username: "highfive", email: "highfive@fake.com", password: "password", id: 100)
         @lowfive = User.create!(username: "lowfive", email: "lowfive@fake.com", password: "password", id: 101)
         @sidefive = User.create!(username: "sidefive", email: "sidefive@fake.com", password: "password", id: 102)
@@ -20,44 +20,45 @@ describe "Movie Show Page" do
         fill_in :username, with: "highfive"
         fill_in :password, with: "password"
         click_button "Log In"
-
-    visit movie_path(@movie.id)
-  end
-
-  describe "When I visit the movie's detail page," do
-    it "I should see a button to create a viewing party", :vcr do
-      within '.viewing-party' do
-        expect(page).to have_button('Create Viewing Party for Movie')
-        click_button
       end
+      visit movie_path(@movie.id)
     end
 
-    describe "And I should see the following information about the movie:" do
-      it "Movie information", :vcr do
-        expect(page).to have_content(@movie.title)
-        expect(page).to have_content(@movie.vote_average)
-        expect(page).to have_content("#{@movie.duration/60} hours " + "#{@movie.duration%60}")
-        expect(page).to have_content(@movie.overview)
+    describe "When I visit the movie's detail page," do
+      it "I should see a button to create a viewing party", :vcr do
+        within '.viewing-party' do
+          expect(page).to have_button('Create Viewing Party for Movie')
+          click_button
+        end
       end
 
-      it "Genre(s) associated with the movie", :vcr do
-        genres = @movie.genres.map {|genre| genre[:name]} if @movie.genres
-        expect(page).to have_content(genres.first)
-        expect(page).to have_content(genres.last)
-      end
+      describe "And I should see the following information about the movie:" do
+        it "Movie information", :vcr do
+          expect(page).to have_content(@movie.title)
+          expect(page).to have_content(@movie.vote_average)
+          expect(page).to have_content("#{@movie.duration/60} hours " + "#{@movie.duration%60}")
+          expect(page).to have_content(@movie.overview)
+        end
 
-      it "List of the first 10 cast members and characters they play", :vcr do
-        expect(page).to have_content(@movie.cast.first.name)
-        expect(page).to have_content(@movie.cast.first.character)
-        expect(page).to have_content(@movie.cast[9].name)
-        expect(page).to have_content(@movie.cast[9].character)
-      end
+        it "Genre(s) associated with the movie", :vcr do
+          genres = @movie.genres.map {|genre| genre[:name]} if @movie.genres
+          expect(page).to have_content(genres.first)
+          expect(page).to have_content(genres.last)
+        end
 
-      it "Count of reviews and review contents", :vcr do
-        expect(page).to have_content("#{@movie.reviews.count} Review(s)")
-        review1 = @movie.reviews.first
-        expect(page).to have_content(review1.author)
-        expect(page).to have_content(review1.content)
+        it "List of the first 10 cast members and characters they play", :vcr do
+          expect(page).to have_content(@movie.cast.first.name)
+          expect(page).to have_content(@movie.cast.first.character)
+          expect(page).to have_content(@movie.cast[9].name)
+          expect(page).to have_content(@movie.cast[9].character)
+        end
+
+        it "Count of reviews and review contents", :vcr do
+          expect(page).to have_content("#{@movie.reviews.count} Review(s)")
+          review1 = @movie.reviews.first
+          expect(page).to have_content(review1.author)
+          expect(page).to have_content(review1.content)
+        end
       end
     end
   end
