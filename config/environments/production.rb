4f7f6ba5d7a536880a309b0f72d7c3c4582fc26f
Jenny Branham <branhamjenny@gmail.com>
2021-03-31 15:14:41 -0500

    Production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://morning-savannah-16693.herokuapp.com' }
+  config.action_mailer.smtp_settings = { :address => "morning-savannah-16693.herokuapp.com", :port => 1025 }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
